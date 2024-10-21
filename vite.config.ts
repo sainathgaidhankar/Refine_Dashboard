3d15503bbb3a562e190b97bf6dbc0239f1a87909
sainath gaidhankar <sainathgaidhankar@gmail.com>
2024-10-21 19:41:14 +0530

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
     plugins: [tsconfigPaths(),react()],
-    base: "/dashboard/",
+    base: "/refine_dashboard/",
 });
 
 
