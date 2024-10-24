f4208a8f1a49e2208e56ebca3e3530d58b0749ad
sainath gaidhankar <sainathgaidhankar@gmail.com>
2024-10-24 13:18:35 +0530

    add: deploy workflow

--- src/config/resources.tsx
+++ src/config/resources.tsx
@@ -12,7 +12,7 @@ export const resources: IResourceItem[] = [
     list: "/",
     meta: {
       label: "Dashboard",
-      // @ts-expect-error Ant Design Icon's v5.0.1 has an issue with @types/react@^18.2.66
+          
       icon: <DashboardOutlined />,
     },
   },
@@ -24,7 +24,7 @@ export const resources: IResourceItem[] = [
     edit: "/companies/edit/:id",
     meta: {
       label: "Companies",
-      // @ts-expect-error Ant Design Icon's v5.0.1 has an issue with @types/react@^18.2.66
+          
       icon: <ShopOutlined />,
     },
   },
@@ -35,7 +35,7 @@ export const resources: IResourceItem[] = [
     edit: "/tasks/edit/:id",
     meta: {
       label: "Tasks",
-      // @ts-expect-error Ant Design Icon's v5.0.1 has an issue with @types/react@^18.2.66
+          
       icon: <ProjectOutlined />,
     },
   },
